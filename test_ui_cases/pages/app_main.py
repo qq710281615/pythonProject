e7e30d547cee2534f627161858d764d712d2524b
qq710281615 <710281615@qq.com>
2022-05-22 19:34:28 +0800

    first

--- test_ui_cases/pages/app_main.py
+++ test_ui_cases/pages/app_main.py
@@ -1,5 +1,10 @@
 import time
+import uuid
+from datetime import datetime
+
 from appium import webdriver
+from appium.webdriver.extensions.android.gsm import GsmCallActions
+
 from test_tool import read_yaml_file
 from test_tool import my_log
 from test_ui_cases.pages.base_page import BasePage
@@ -27,4 +32,31 @@ class App(BasePage):
         return self
 
     def homepage(self):
+        # 设置网络
+        self._driver.set_network_connection(4)
+        time.sleep(3)
+        self.save_picture()
+        self._driver.start_recording_screen()
         self.steps("../test_case/test_tep.yml")
+        time.sleep(3)
+        self._driver.set_network_connection(1)
+        time.sleep(3)
+        self.save_picture()
+        self._driver.set_network_connection(0)
+        time.sleep(3)
+        self.save_picture()
+        self._driver.set_network_connection(4)
+        time.sleep(3)
+        self.save_picture()
+        self._driver.set_network_connection(6)
+        time.sleep(3)
+        self.save_picture()
+
+    def save_picture(self):
+        today = datetime.today().date()
+        filename = str(uuid.uuid4()) + str(today) + ".png"
+
+        self._driver.get_screenshot_as_file(filename)
+
+
+
