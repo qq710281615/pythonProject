df43d190075ec01671afaed700d64a84c2044210
qq710281615 <710281615@qq.com>
2022-06-01 18:37:41 +0800

    first

--- ui_test/test_case/conftest.py
+++ ui_test/test_case/conftest.py
@@ -1,9 +1,18 @@
-import os
+# import os
 
 import pytest
 
 
-@pytest.fixture(scope="module")
-def generate_report():
-    print("开始测试")
+@pytest.fixture(scope="module", autouse=True)
+def c():
+    print("开始测试模块")
+    yield
+    print("模块测试结束")
+    # report_path = r"C:\Users\ssbai\PycharmProjects\pythonProject\ui_test\report\{0}".format(20220547)
+    # if os.path.exists(report_path) is False:
+    #     os.makedirs(report_path)
+    #     cmd_str = "allure generate ../allure_temp -o {0} --clean".format(report_path)
+    #     os.system(cmd_str)
+    #     print("报告生成完成")
+
 
